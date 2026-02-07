c37e1e1f871127fa21832d5f5733027eb2f33b70
EnisCiftci <eniscif@googlemail.com>
2026-02-07 23:55:40 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { m } from "$lib/paraglide/messages";
+    import { setLocale } from "$lib/paraglide/runtime";
+</script>
+
+<h1>{m.hello_world({ name: "Asobi" })}</h1>
+<h1>{m.aqua_heavy_seahorse_persist()}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => setLocale("de")}>German</button>
+<button onclick={() => setLocale("en")}>English</button>
