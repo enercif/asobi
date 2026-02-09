9bd5516bd72b6a249c15253bd421fd68c351bdec
EnisCiftci <eniscif@googlemail.com>
2026-02-09 13:43:29 +0100

    feature: added girid list

---
+++ src/lib/ui/game-grid.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { Game } from "$lib/types/game.type";
+
+    interface Props {
+        games: Game[];
+    }
+
+    let { games }: Props = $props();
+</script>
+
+<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 h-full pb-10 overflow-y-scroll">
+    {#each games as game}
+        <a
+            href={game.link}
+            class="rounded-2xl bg-[#A6FAFF] aspect-2/3 flex items-center justify-center text-xl font-bold"
+        >
+            {game.name}
+        </a>
+    {/each}
+</div>
