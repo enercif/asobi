89d750642842a89e6210a509dc6597f071d60710
EnisCiftci <eniscif@googlemail.com>
2026-03-22 16:48:54 +0100

    feat(ui): labeled work in progress sites

--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -1,1 +1,10 @@
-<h1>messages</h1>
+<script>
+    import { ConstructionIcon } from "@lucide/svelte";
+</script>
+
+<div class="flex size-full items-center justify-center pb-30">
+    <p class="text-center text-5xl font-bold">Work in Progress</p>
+    <ConstructionIcon
+        size={48}
+        class="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 scale-500  text-primary/25" />
+</div>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,8 +1,10 @@
-<script lang="ts">
-    import { setLocale } from "$lib/paraglide/runtime";
+<script>
+    import { ConstructionIcon } from "@lucide/svelte";
 </script>
 
-<div class="flex size-full flex-col gap-8">
-    <button onclick={() => setLocale("de")}>German</button>
-    <button onclick={() => setLocale("en")}>English</button>
+<div class="flex size-full items-center justify-center pb-30">
+    <p class="text-center text-5xl font-bold">Work in Progress</p>
+    <ConstructionIcon
+        size={48}
+        class="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 scale-500  text-primary/25" />
 </div>
